6e95107a582f26580fc72c77380b53a1f8335af9
IsaacDSC <isaac8.silva@hotmail.com>
2020-02-10 16:25:13 -0300

    upgrades

--- routes/exibePedidos.js
+++ routes/exibePedidos.js
@@ -3,7 +3,13 @@ const router = express.Router()
 const Post = require('../models/Post')
 
 router.get('/', (req, res) => {
-    Post.findAll().then(function(posts) {
+    //ASC  == DO MAIS NOVO PARA O ANTIGO 
+    //DESC == DO MAIS VELHO PARA O NOVO
+    Post.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(function(posts) {
         res.render('exibePedidos/exibePedidos', { posts: posts })
     })
 })
